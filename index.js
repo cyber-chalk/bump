c8ea036a14d434c59afa4912cfaeec6b79d5881a
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2023-09-16 13:55:28 +0000

    ws not working

--- index.js
+++ index.js
@@ -5,36 +5,9 @@ app.use(express.static("public")); // uses the public folder for static files
 
 app.listen(3000);
 
-const {WebSocketServer} = require("ws")
+const { WebSocketServer } = require("ws");
 
 const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", function connection(ws) {
-	ws.on("error", console.error);
-
-	ws.on("message", function message(data) {
-		console.log("received: %s", data);
-	});
-
-	ws.send("something");
-});
-
-// function naiveBetaPDF(x, a, b) {
-// 	// Naive implementation of the beta pdf function
-// 	// Using factorials
-// 	return (Math.pow(x, a - 1) * Math.pow(1 - x, b - 1)) / naiveBetaFunc(a, b);
-// }
-// function naiveBetaFunc(a, b) {
-// 	// Naive implementation of the beta function
-// 	// using factorials
-// 	return (factorial(a - 1) * factorial(b - 1)) / factorial(a + b - 1);
-// }
-// function factorial(x) {
-// 	if (x === 0) {
-// 		return 1;
-// 	}
-// 	return x * factorial(x - 1);
-// }
 
 // for (let i = 0; i < 100; i++) {
 // 	// let rand = Math.floor(Math.random() * (10 - 1) + 1) / 10;
@@ -83,30 +56,26 @@ function expensive(rand, i) {
 	// }
 }
 
-let bank = 1000;
+let bank = 100;
 
 function getRandom(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-let previousArr = [];
-function previousGames(multipler) {
-	previousArr.push(multipler);
-}
+// let previousArr = [];
+// function previousGames(multipler) {
+// 	previousArr.push(multipler);
+// }
 
 async function game(amountPayed) {
-	bank -= amountPayed; // update this in frontend
+	bank -= amountPayed;
 	let exitArr = [];
 	let players = 100;
 	let multiplier = 100;
-	setTimeout(() => {
-		// insert the bet amount through websocket
-		// use await
-	}, 5000);
-
-	let rpd = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+	// no 5 second wait
+	let rpd = Math.floor(getRandom(1, 100));
 	//prettier-ignore
-	if (rpd > 95) return crash(1.00); // might need to change "return"
+	if (rpd > 95) return crash(100);
 
 	for (let i = 0; i < 100; i++) {
 		exitArr.push(betaPDF(i / players, 2, 4));
@@ -134,36 +103,50 @@ async function game(amountPayed) {
 	let lapsed = 0; // time elapsed since last exit
 	let n = 0; // counter
 	let leave = 0; //adds beta distrobution and when 1 person has been accounted for it leaves
-	setInterval(() => {
-		if (multiplier == 100) multiplier += 1;
-		multiplier += 1;
-		amountPayed *= multipler / 100;
 
-		exitArr[n] += leave;
-		if (leave >= 1) {
-			leave--;
-			lapsed = 0;
-		}
-		n++;
-		lapsed += 50;
-		tick(lapsed, multiplier, amountPayed);
-	}, 50);
+	wss.on("listening", function (ws) {
+		ws.send("start");
+
+		let intID = setInterval(() => {
+			if (multiplier == 100) multiplier += 1;
+			multiplier += 1;
+			amountPayed *= multipler / 100;
+
+			exitArr[n] += leave;
+			if (leave >= 1) {
+				leave--;
+				lapsed = 0;
+			}
+			n++;
+			lapsed += 50;
+			tick(lapsed);
+			if (lapsed != 0) {
+				clearInterval(intID);
+				return ws.send(`crash ${multiplyer}`);
+			}
+		}, 50);
+	});
 }
 
-function tick(probability, multiplier, amountPayed) {
+function tick(probability) {
 	probability = (probability * 2) / 100;
 	if (getRandom(0, 10) + probability >= 10) {
 		// change number pls
-		return crash(multiplier, amountPayed);
+		//crash
+		return;
 	} else {
 		return (lapsed = 0);
 	}
 }
 
-function crash(multiplier, amountPayed) {
-	previousGames(multiplier);
-	setTimeout(() => {
-		// pause game and show multiplyer
-	}, 3000);
-	return game(20);
-}
+wss.on("connection", function connection(ws) {
+	ws.on("error", console.error);
+
+	ws.on("message", function message(data) {
+		console.log("received:", data);
+		let data1 = data.split(" ");
+		if (data1[0] == "bet") game(data1[1]);
+	});
+
+	ws.send(`bank ${bank}`);
+});
--- public/index.js
+++ public/index.js
@@ -1,10 +1,52 @@
 let bank = document.getElementById("bank").innerText;
+let bankNum = bank.substring(1);
 let betPlaced = document.getElementById("betPlaced").value;
 let button = document.getElementById("button");
+let multiplyerDisp = document.getElementById("bigText").innerText;
+
+const socket = new WebSocket("ws://localhost:8080");
+socket.onerror = (event) => {
+	console.log(event);
+};
+
+
+socket.addEventListener("message", (event) => {
+	let data1 = event.data.split(" ");
+	if (data1[0] == "bank") {
+		document.getElementById("bank").innerText = `$${data1[1]}`;
+		bankNum = data1[1];
+	}
+	if (data1[0] == "start") {
+		button.style.color = "#fff7ef";
+		button.style.backgroundColor = "rgb(20, 20, 20)";
+
+		let multiplyer = 100;
+		let MID = setInterval(() => {
+			multiplyerDisp = (multiplyer / 100).toFixed(2);
+			multiplyer++;
+		}, 50);
+	}
+	if (data1[0] == "crash") {
+		document.getElementsByClassName("prevGame").innerText = data1[1];
+		clearInterval(MID);
+		multiplyerDisp = "0.00";
+		button.style = "";
+	}
+});
 
 button.addEventListener("click", function () {
-	console.log(bank, betPlaced);
-	if (bank - betPlaced <= 0) return;
-	bank = bank - betPlaced;
+	console.log(bankNum, betPlaced);
+	if (bankNum - betPlaced <= 0 || bankNum - betPlaced == NaN) return;
+	bankNum -= betPlaced;
+	document.getElementById("bank").innerText = `$${bankNum}`;
+	console.log(bank);
 	button.style.backgroundColor = "#7b4d2b";
+	button.children[0].innerHTML = "waiting...";
+	console.log(button.children[0].innerHTML);
+
+	socket.send(`bet ${betPlaced}`);
 });
+
+//steps
+// hook up web sockets, 🟢 bank, 🟢 game start, 🟢timer, 🟢 multiplyer start, cashout and game end
+// add a true/false thing so when you press the button while the game is runnign
